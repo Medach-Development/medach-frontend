09f873f54ebe865c87a790ee5794d7dfff4e3890
Halla Mahyn <hallamahyn@gmail.com>
2021-05-11 21:17:30 +0300

    some fix for slider

--- components/TheSlider.vue
+++ components/TheSlider.vue
@@ -52,8 +52,22 @@ export default {
     },
 
     getSlideImageUrl(slide) {
-      return _.get(slide, slide.sliderImage.url, slide.coverImage.url)
+      if (!slide) {
+        return ''
+      }
+      else if (slide.sliderImage && slide.sliderImage.url) {
+        return slide.sliderImage.url
+      } else {
+        return slide.coverImage.url || ''
+      }
     }
+    // LODASH PODVODIT VOT TUT VOT
+    // getSlideImageUrl(slide) {
+    //   console.log("SLIDE EXPECTED", slide.sliderImage.url)
+    //   console.log("RESULT", _.get(slide, slide.sliderImage.url, slide.coverImage.url))
+    //   console.log("__________________________________________________________________")
+    //   return _.get(slide, slide.sliderImage.url, slide.coverImage.url)
+    // }
   }
 }
 </script>
